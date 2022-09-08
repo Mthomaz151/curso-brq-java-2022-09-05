3d7e5b7d27642c3daa68b331d27b2fef01b49801
Mthomaz151 <112988948+Mthomaz151@users.noreply.github.com>
2022-09-08 19:45:31 -0300

    Atualização via WEB GIT HUB

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,6 +1,6 @@
 public class OlaMundo{
    public static void main(String [] args){
       System.out.println("Olá Mundo Novo!");
-      //Alteração aula interface WEB
+      //Alteração aula interface WEB new
    }
 }

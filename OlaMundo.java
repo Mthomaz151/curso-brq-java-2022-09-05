14d3f61e77ee6c95d5df8f15c104e44319fd842a
Marcelo Thomaz <marcelo.thomaz@hotmail.com>
2022-09-08 21:38:22 -0300

    alterando arquivo branch main

--- OlaMundo.java
+++ OlaMundo.java
@@ -2,5 +2,6 @@ public class OlaMundo{
    public static void main(String [] args){
       System.out.println("Olá Mundo Novo!");
       //Alteração aula interface WEB new
+         System.out.println("Alterado!");      
    }
 }
